e5488ea9d509d0c9589570667a15ef5fd98eb0a8
Nikolai0902 <kbus94@yandex.ru>
2023-11-07 18:53:33 +0300

    2. Регистрация пользователя

--- src/main/java/ru/job4j/accidents/service/UserServiceSD.java
+++ src/main/java/ru/job4j/accidents/service/UserServiceSD.java
@@ -1,6 +1,8 @@
 package ru.job4j.accidents.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.UserRepositorySD;
@@ -9,11 +11,18 @@ import java.util.Optional;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserServiceSD {
 
     private final UserRepositorySD userRepository;
 
     public Optional<User> save(User user) {
-        return Optional.of(userRepository.save(user));
+        Optional<User> optionalUser = Optional.empty();
+        try {
+            optionalUser = Optional.of(userRepository.save(user));
+        } catch (DataIntegrityViolationException e) {
+            log.error("Пользователь с этим username уже существует", e);
+        }
+        return optionalUser;
     }
 }
